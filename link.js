cc9472a7c734170cac5c46f4458b9941f77d2d0a
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-11-15 19:04:44 +0600

    added

--- link.js
+++ link.js
@@ -1,6 +1,6 @@
 class HomeComponent extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<a href="./index.html">Home</a>`;
+    this.innerHTML = `<a href="./index.html">&#8592; Back to Home</a>`;
   }
 }
 
